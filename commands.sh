735feda60274bd1498bd8338941770db24cafe78
Michal Jan Warecki <michal.jan.warecki@nhn.no>
2025-10-28 15:11:46 +0100

    dotfiles install

--- commands.sh
+++ commands.sh
@@ -1,10 +1,15 @@
+# brew list --installed-on-request
 WANTED="cloc docker fx htop jq lazygit lsd make neovim slides viddy"
 
-_install() {
+dotfiles_install() {
   if [ $(which brew) -ne 0 ]; then
     echo "did not find brew"
     exit 1
   fi
 
-  brew install $WANTED
+  brew install ${=WANTED}
+}
+
+dotfiles_brew() {
+
 }
