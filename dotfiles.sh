b46330d7f40046a31400a2c6eedc74db7950c61a
Michal Jan Warecki <michal.jan.warecki@nhn.no>
2025-10-28 15:11:53 +0100

    dotfiles install

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,8 +1,11 @@
 dotfiles() {
   # echo "Dotfiles!"
 
-  if [ $1 = "list" ]; then
+  if [[ $1 == "list" ]]; then
     echo "Included files:\n"
     cat $BASE/main.sh | grep source | sed -E 's/^.*\/(.*\.sh$)/\1/'
+  elif [[ $1 == "install" ]]; then
+    shift
+    dotfiles_install $@
   fi
 }
