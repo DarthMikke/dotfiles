6f89373c87f6bdffaedefd77b77a271a48eb6f4e
Michal Jan Warecki <mwarecki1@gmail.com>
2025-10-30 09:11:10 +0100

    shell.sh: prompt variable and PATH

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 BASE=$XDG_CONFIG_HOME/dotfiles
 
 source $BASE/dotfiles.sh
+source $BASE/shell.sh
 source $BASE/commands.sh
 source $BASE/aliases.sh
 source $BASE/jd.sh
---
+++ shell.sh
@@ -0,0 +1,8 @@
+# https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
+# PS1=$'%B%n@%m%b %40<..<%~%<<%20(l.\n. )%0(?.%F{green}.%F{red})%B%#%b%f '
+# PS1=$'%F{145}%n@%f%B%m%b %F{146}%40<..<%~%<<%f%20(l.\n. )%0(?.%F{green}.%F{red})%B%#%b%f '
+PS1=$'%F{145}%n@%f%B%m%b %F{146}%-20<..<%~%<<%f%30(l.\n. )%0(?.%F{green}.%F{red})%B%#%b%f '
+
+
+export PATH="$PATH:/$HOME/.local/bin"
+
