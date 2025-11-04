c5fd2c684cf42fa57630868cedfddf2a12bb6c0f
Michal Jan Warecki <michal.jan.warecki@nhn.no>
2025-11-04 12:24:04 +0100

    Shell completions

--- shell.sh
+++ shell.sh
@@ -1,3 +1,14 @@
+# Completions
+autoload -Uz compinit
+compinit
+
+# Completion style: display menu below the prompt
+zstyle ':completion:*' menu yes select
+
+# autocompletion using arrow keys (based on history)
+bindkey '\e[A' history-search-backward
+bindkey '\e[B' history-search-forward
+
 # https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
 # PS1=$'%B%n@%m%b %40<..<%~%<<%20(l.\n. )%0(?.%F{green}.%F{red})%B%#%b%f '
 # PS1=$'%F{145}%n@%f%B%m%b %F{146}%40<..<%~%<<%f%20(l.\n. )%0(?.%F{green}.%F{red})%B%#%b%f '
