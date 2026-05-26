23dacb80d698a87e6a816bb829db3646bd39b11c
Michal Jan Warecki <michal.jan.warecki@nhn.no>
2026-05-26 13:36:44 +0200

    Transparent background

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,24 @@
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   name = "kanagawa",
+--   opts = {
+--     transparent = true,
+--   },
+--   config = function(_, opts)
+--     require("kanagawa").setup(opts)
+--     vim.cmd.colorscheme("kanagawa")
+--   end,
+-- }
+
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
